12b1f7bb1e6e2a21e7f3c052a0eb1fe68fab4b45
lit95 <taylorli95@gmail.com>
2020-10-24 21:09:13 -0400

    updated lab 6

---
+++ public/lab_6/script.js
@@ -0,0 +1,64 @@
+// You may wish to find an effective randomizer function on MDN.
+
+const { reverse } = require("cypress/types/lodash");
+
+function range(int) {
+  const arr = [];
+  for (let i = 0; i < int; i += 1) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+function sortFunction(a, b, key) {
+  if (a[key] < b[key]) {
+    return -1;
+  } if (a[key] > b[key]) {
+    return 1;
+  }
+  return 0;
+}
+
+async function handleEvent(evt) {
+  const results= await fetch('/api');
+  const json=results.json();
+  console.log('jsonFromServer', json);
+  const reverseList=json.sort((a,b)=>sortFunction(b,a,'name'));
+
+  reverseList.array.forEach(element=> {
+    const li=document.createElement('li')
+    document.append(li);
+  })
+}
+
+const element = await(await arrayPromise()).split(' ');
+
+document.body.addEventListener('submit', async (e) => {
+  e.preventDefault(); // this stops whatever the browser wanted to do itself.
+  const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
+  fetch('/api', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(form)
+  })
+    .then((fromServer) => fromServer.json())
+    .then((fromServer) => {
+      console.log('jsonFromServer', jsonFromServer);
+      const reverseList=newArr2.sort((a,b)=>sortFunction(b,a,'name'));
+      
+      //.catch((err)) => {
+      //  console.log(err)
+      //}
+      if (document.querySelector('.flex-inner')) {
+        document.querySelector('.flex-inner').remove();
+      }
+      const newArr=range(10);
+      const newArr2=newArr.map(() => {
+        const number=getRandomIntInclusive(0,243);
+        return fromServer[number];
+      });
+    })
+    .catch((err) => console.log(err));
+});
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
+import countries from './public/lab_6/countries.js'
 
 dotenv.config();
 
@@ -13,6 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
@@ -21,6 +28,7 @@ app.route('/api')
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
+    res.json(countries);
   });
 
 app.listen(port, () => {
